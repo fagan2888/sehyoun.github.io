4ca0c15845bf686286d445f833153244bd9cefa0
SeHyoun Ahn <sehyoun.ahn@gmail.com>
2017-04-01 19:02:38 -0400

    fixed broken links

--- static/js/responsive.js
+++ static/js/responsive.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
   $("#main_nav").css("display","block");
   $("body").css("padding-top","50px");
   if ( $(window).width() < 800) {
-    $("#main-picture").attr("src","pics/main_pic_md.jpg");
+    $("#main-picture").attr("src","static/img/main_pic_md.jpg");
   }
   else {
-    $("#main-picture").attr("src","pics/main_pic_lg.jpg");
+    $("#main-picture").attr("src","static/img/main_pic_lg.jpg");
   }
 });
 
 $( window ).resize(function() {
-  if (($(window).width() >= 800) && ($("main-picture").attr("src")!="pics/main_pic_lg.jpg")) {
-    $("#main-picture").attr("src","pics/main_pic_lg.jpg");
+  if (($(window).width() >= 800) && ($("main-picture").attr("src")!="static/img/main_pic_lg.jpg")) {
+    $("#main-picture").attr("src","static/img/main_pic_lg.jpg");
   }
 });
